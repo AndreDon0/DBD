a93e291c14bd0ce1a4c2dfc56f23f3ef7e4b2e02
AndreDon0 <andrey.donetskov@gmail.com>
2024-07-17 17:30:04 +0300

    beta 2

--- circle.py
+++ circle.py
@@ -1,13 +1,13 @@
 import numpy as np
 import cv2
-from time import time
-from math import sin, cos, pi
+from math import sin, cos, pi, inf
 
-SIZE = 360
-WHITE = 100
-RED = 200
-GOOD = 10
-EXCELLENT = 250
+SIZE = 180  # Размер массива круга
+WHITE = 100  # С какой интенсивности цвета мы его считаем белым
+RED = 200  # С какой интенсивности цвета мы его считаем красным
+GOOD = 10  # Если интенсивность цвета ниже, то считаем эту зону хорошей проверкой реакции
+EXCELLENT = 250  # Если интенсивность цвета выше, то считаем эту зону хорошей проверкой реакции
+CORRECT_LENGTH = int(SIZE / 60)  # Длинна при которой мы считаем скопление точек регионом
 
 
 # Круг, представимый в виде массива
@@ -17,91 +17,96 @@ class Circle:
             centre: list,
             radius: int,
             t: float,
-            size: int = SIZE,
+            size: int,
     ) -> None:
         self.centre = centre
         self.radius = radius
-        self.circle = np.empty(SIZE)
         self.size = size
         self.t = t
+        self.circle = np.empty(size)
 
     # Из индекса массива возвращает угол
     def from_size_to_phi(self, i: int) -> float:
-        return i / self.size * 2 * pi
+        return i % self.size / self.size * 2 * pi
+
+    # Из массива индексов возвращает массив углов
+    def from_areas_to_phi(self, areas: list) -> list:
+        for i in range(0, len(areas)):
+            areas[i][0] = self.from_size_to_phi(areas[i][0])
+            areas[i][1] = self.from_size_to_phi(areas[i][1])
+        return areas
 
     # Зная угол, возвращает ЦЕЛОЧИСЛЕННЫЕ декартовы координаты
-    def coordinate(self, pfi: int) -> list:
+    def coordinate(self, pfi: float) -> list:
         return [self.centre[0] + int(self.radius * cos(pfi)),
                 self.centre[1] + int(self.radius * sin(pfi))]
 
     # Из картинки читает линию круга
-    def circling_image(self, image):
+    def circling_image(self, image: np.ndarray) -> None:
         for i in range(0, self.size):
             phi = self.from_size_to_phi(i)
             y, x = self.coordinate(phi)
             self.circle[i] = image[y, x]
 
-    def to_array(self) -> np.ndarray:
-        return self.circle
-
-    # Возвращает максимальный по длине массив массива
+    # Вывод индексов подходящих участков
     @staticmethod
-    def max_len(arr: list) -> list:
-        m = []
-        for i in arr:
-            if len(i) > len(m):
-                m = i
-        return m
+    def find_true_areas(
+            array: np.ndarray,
+            cor_len: int
+    ) -> list:
+        true_regions = []
+        area = [0, 0]
+        array = np.append(array, [False])
+
+        beg = False
+        for i, bool_value in enumerate(array):
+            if beg:
+                if bool_value:
+                    area[1] = i
+                else:
+                    if area[1] - area[0] + 1 > cor_len:
+                        true_regions.append(area.copy())
+                    beg = False
+            else:
+                if bool_value:
+                    beg = True
+                    area[0] = i
+                    area[1] = i
+
+        return true_regions
 
     # Если в какой-то области круга значения больше чем
     # введённое, вывести начало и конец наибольшей области
-    def more_then(self, other: int) -> float:
+    def more_then(
+            self,
+            other: int,
+            cor_len: int = CORRECT_LENGTH
+    ) -> list:
         areas = self.circle > other
-        max_area = [0, 0]
-        area = [0, 0]
-        p = False
-        for i, bool_value in enumerate(areas):
-            if bool_value and p:
-                area[0] = i
-                p = True
-            else:
-                area[1] = i - 1
-                p = False
-                if area[1] - area[0] > max_area[1] - max_area[0]:
-                    max_area = area
-
-        max_area[0] = self.from_size_to_phi(max_area[0])
-        max_area[1] = self.from_size_to_phi(max_area[1])
-        return (max_area[0] + max_area[1]) / 2
+        true_regions = self.find_true_areas(areas, cor_len=cor_len)
+        true_regions_phi = self.from_areas_to_phi(true_regions)
+        return true_regions_phi
 
     # Если в какой-то области круга значения меньше чем
     # введённое, вывести начало и конец наибольшей области
-    def less_then(self, other: int) -> float:
+    def less_then(
+            self,
+            other: int,
+            cor_len: int = CORRECT_LENGTH
+    ) -> list:
         areas = self.circle < other
-        max_area = [0, 0]
-        area = [0, 0]
-        p = False
-        for i, bool_value in enumerate(areas):
-            if bool_value and p:
-                area[0] = i
-                p = True
-            else:
-                area[1] = i - 1
-                p = False
-                if area[1] - area[0] > max_area[1] - max_area[0]:
-                    max_area = area
-
-        max_area[0] = self.from_size_to_phi(max_area[0])
-        max_area[1] = self.from_size_to_phi(max_area[1])
-        return (max_area[0] + max_area[1]) / 2
+        true_regions = self.find_true_areas(areas, cor_len=cor_len)
+        true_regions_phi = self.from_areas_to_phi(true_regions)
+        return true_regions_phi
 
 
 class DbDCircle:
     # Возвращает центр круга и его радиус
     @staticmethod
-    def CRcircle(
+    def calc_cen_rad_circle(
             image: np.ndarray,
-            white: int = WHITE
+            white: int = WHITE,
+            math: bool = False
     ) -> list:
 
         height, width = image.shape[0:2]
@@ -116,10 +121,12 @@ class DbDCircle:
                 if i > bottom:
                     bottom = i
 
+        if not math and bottom < height:  # Подгон значений
+            bottom += 1
         x0, y0 = middle, int((top + bottom) / 2)
-        center = [x0, y0]
-        radius = int((bottom - top) / 2)
-        return [center, radius]
+        rad = int((bottom - top) / 2)
+        center = [y0, x0]
+        return [center, rad]
 
     def __init__(
             self,
@@ -129,45 +136,83 @@ class DbDCircle:
     ) -> None:
 
         # Создание разный видов картинки
-        self.Gimg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        b, g, r = cv2.split(image)
-        self.Oimg = (np.array(r, np.int16) - np.array(g, np.int16) - np.array(b, np.int16))
+        self.image = image
+        self.grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        self.object_img = self.create_obg_image(image)
 
         # Задание математических параметров
-        self.center, self.radius = self.CRcircle(self.Gimg)
+        self.center, self.radius = self.calc_cen_rad_circle(self.grey_img)
 
         # Создание круга
         self.size = size
         self.time_start = t
-        self.Gcircle = Circle(self.center, self.radius, t, size)
-        self.Ocircle = Circle(self.center, self.radius, t, size)
-        self.Gcircle.circling_image(self.Gimg)
-        self.Ocircle.circling_image(self.Oimg)
+        self.grey_circle = Circle(self.center, self.radius, t, size)
+        self.object_circle = Circle(self.center, self.radius, t, size)
+        self.grey_circle.circling_image(self.grey_img)
+        self.object_circle.circling_image(self.object_img)
 
         # Задание изначальной позиции объекта
-        self.Oposition = self.Ocircle.more_then(RED)
+        self.object_position = self.object_circle.more_then(RED, cor_len=0)
 
         # Задание позиции хорошей и отличной зон
-        self.Eposition = self.Gcircle.more_then(EXCELLENT)
-        self.Gposition = self.Gcircle.less_then(GOOD)
+        self.excellent_position = self.grey_circle.more_then(EXCELLENT)
+        self.good_position = self.grey_circle.less_then(GOOD)
 
-    def calc_time(self, image: np.ndarray, t: float) -> float:
+    # Создание картинки на которой объект четко выделен
+    @staticmethod
+    def create_obg_image(image: np.ndarray) -> np.ndarray:
         b, g, r = cv2.split(image)
-        new_Oimg = (np.array(r, np.int16) - np.array(g, np.int16) - np.array(b, np.int16))
-        new_Ocircle = Circle(self.center, self.radius, t, self.size)
-        new_Ocircle.circling_image(new_Oimg)
-        new_Oposition = new_Ocircle.more_then(RED)
+        return np.array(r, np.int16) - np.array(g, np.int16) - np.array(b, np.int16)
 
-        phi0 = self.Oposition
-        phi1 = new_Oposition
-        t0 = self.Ocircle.t
-        t1 = t
-        om = (phi1 - phi0) / (t1 - t0)
-        if self.Eposition != 0:
-            return self.Eposition / om
-        elif self.Gposition != 0:
-            return self.Gposition / om
+    # Поиск минимального времени для пересечения указателя и окрестности
+    @staticmethod
+    def find_time_to_areas(areas: list, om: float) -> float:
+        min_time = inf
+        for area in areas:
+            p1 = (area[1] - area[0]) / 2
+            p2 = p1 - 2 * pi
+            t1 = p1 / om
+            t2 = p2 / om
+            tm = min(t1, t2)
+            if min_time < tm:
+                min_time = tm
+
+        return min_time
+
+    def calc_time(self, new_image: np.ndarray, t: float) -> float:
+        b, g, r = cv2.split(new_image)
+        new_object_img = (np.array(r, np.int16) - np.array(g, np.int16) - np.array(b, np.int16))
+        new_object_circle = Circle(self.center, self.radius, t, self.size)
+        new_object_circle.circling_image(new_object_img)
+        new_object_position = new_object_circle.more_then(RED)
+
+        phi0 = self.object_position[0][1] - self.object_position[0][0]
+        phi1 = new_object_position[0][1] - new_object_position[0][0]
+        time0 = self.object_circle.t
+        time1 = t
+        om = (phi1 - phi0) / (time1 - time0)
+
+        if not self.excellent_position == []:
+            return self.find_time_to_areas(self.excellent_position, om)
+        elif not self.good_position == []:
+            return self.find_time_to_areas(self.good_position, om)
         else:
             raise ValueError("Не найдено ни хорошей ни отличной позиции")
 
+    # Проверка круга на то что бы он был из дбд
+    def check_dbd_circle(self) -> bool:
+        check_obj = (not self.object_position == [])
+        check_ex = (not self.excellent_position == [])
+        check_good = (not self.good_position == [])
+        check_circle = bool(sum(self.grey_circle.circle > WHITE) > (SIZE / 4))
 
+        return check_obj and (check_ex or check_good) and check_circle
+
+    # Вывод рисунка с помеченной окружностью
+    def get_image_circle(self) -> np.ndarray:
+        mod_img = self.image
+        for i in range(0, self.grey_circle.size):
+            phi = self.grey_circle.from_size_to_phi(i)
+            y, x = self.grey_circle.coordinate(phi)
+            mod_img[y, x] = 255
+        return mod_img
--- main.py
+++ main.py
@@ -16,14 +16,13 @@ LOG = True
 WIDTH, HEIGHT = GetSystemMetrics(0), GetSystemMetrics(1)  # Высота и ширина экрана(с учётом масштаба)
 WSCR, HSCR = pag.size()  # Высота и ширина экрана
 CENTER = [int(WSCR / 2), int(HSCR / 2)]  # Центр экрана
-SIZE = 360  # Размер круга
 WHITE = 100  # С какой интенсивности цвета мы его считаем белым
-RED = 100  # С какой интенсивности цвета мы его считаем красным
 
 
 # Проверка нажатия CapsLk
 def is_capslock_on():
     return True if ctypes.WinDLL("User32.dll").GetKeyState(0x14) else False
+
 
 # Нажатие клавиши
 def tap(k):
@@ -31,26 +30,23 @@ def tap(k):
     sleep(random() / 50)
     key.release(k)
 
+
 #  Сделать скриншот
-def take_screen(scr_region=None):
-    if scr_region is None:
-        scr_region = [0, 0, WIDTH, HEIGHT]
+def take_screen(screenshot_region=None):
+    if screenshot_region is None:
+        screenshot_region = [0, 0, WIDTH, HEIGHT]
     while True:
-        scr = camera.grab(region=(scr_region[0], scr_region[1], scr_region[2], scr_region[3]))
-        if scr is not None:
-            return scr
+        screenshot = camera.grab(region=(screenshot_region[0], screenshot_region[1],
+                                         screenshot_region[2], screenshot_region[3]))
+        if screenshot is not None:
+            return screenshot
 
 
 if LOG:
-    # Поиск пути до проекта и создание допустимой временной строки
-    """
-    path = abspath(__file__).replace('\\', '/')
-    path = path[:path.rfind("/")]
-    """
-    t = str(datetime.datetime.now()).replace(':', '-')
-
-    # Создание рабочих папок
-    os.makedirs(f'Screenshots/{t}', exist_ok=True)
+    # Создание допустимой временной строки
+    system_time = str(datetime.datetime.now()).replace(':', '-')
+    # Создание папки для хранения скриншотов
+    os.makedirs(f'Screenshots/{system_time}', exist_ok=True)
 
 key.wait("Caps Lock")
 camera = dxcam.create(output_color="BGR")
@@ -63,6 +59,8 @@ while True:
 
         scr = take_screen(region)
         scr_time = time()
+        if LOG:
+            cv2.imwrite(f"Screenshots/{system_time}/Scr_{step}.png", scr)
 
         height, width = scr.shape[0:2]
         middle = int(width / 2)
@@ -70,18 +68,20 @@ while True:
         mask = column > WHITE
         if np.any(mask):
             cir = DbDCircle(scr, scr_time)
-            sleep(0.01)
-            new_scr = take_screen(region)
-            new_scr_time = time()
-            time_to_tap = cir.calc_time(new_scr, new_scr_time)
+            if cir.check_dbd_circle():
+                sleep(0.01)
+                new_scr = take_screen(region)
+                new_scr_time = time()
+                time_to_tap = cir.calc_time(new_scr, new_scr_time)
 
-            if LOG:
-                cv2.imwrite("Scr.png", scr)
-            sleep(time_to_tap)
-            tap("space")
-            print(time_to_tap)
+                sleep(time_to_tap)
+                tap("space")
+                if LOG:
+                    os.makedirs(f'Screenshots/{system_time}/Step{step}', exist_ok=True)
+                    cv2.imwrite(f"Screenshots/{system_time}/Step{step}/circle.png", cir.get_image_circle())
+                print(time_to_tap)
 
         time_end = time()
         step += 1
         step_time = time_end - time_start
-        print(f"Step {step}. FPS: {1 / step_time} Execution time: {step_time}")
+        print(f"Step {step}. FPS: {1 / step_time}")
